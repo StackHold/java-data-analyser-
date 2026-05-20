5bfaf660250d8f977e728f6c8eb75309af60e8de
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-05-20 15:36:32 -0300

    new: classe reuniao

---
+++ src/br/com/fiap/bean/Reuniao.java
@@ -0,0 +1,12 @@
+package br.com.fiap.bean;
+
+import java.time.LocalDate;
+
+public class Reuniao {
+    private LocalDate data;
+    private String transcricao;
+    private Funcionario vendedor;
+    private Cliente cliente;
+
+
+}
