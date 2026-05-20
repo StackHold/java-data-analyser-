70c6c8958f2f610eb1be0537d8260963bb72410f
João Vitor P.R. <jvprocha.05gmail.com>
2026-05-20 16:13:41 -0300

    Colocando elementos dentro da classe Resultado

--- src/br/com/fiap/bean/AnalisadorTranscricao.java
+++ src/br/com/fiap/bean/AnalisadorTranscricao.java
@@ -1,5 +1,26 @@
 package br.com.fiap.bean;
 
 public class AnalisadorTranscricao {
+    private String palavraChave;
     private int pontuacao;
+
+    //Criando construtores
+    public AnalisadorTranscricao() {}
+
+    //Criando getters e setters
+    public String getPalavraChave() {
+        return palavraChave;
+    }
+    public void setPalavraChave(String palavraChave) {
+        this.palavraChave = palavraChave;
+    }
+    public int getPontuacao() {
+        return pontuacao;
+    }
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    //Criando metodos
+
 }
--- src/br/com/fiap/bean/ResultadoAnalise.java
+++ src/br/com/fiap/bean/ResultadoAnalise.java
@@ -1,7 +1,40 @@
 package br.com.fiap.bean;
 
 public class ResultadoAnalise {
+    //Criando atributos da classe
     private int pontuacao;
     private String classificacao;
     private boolean riscoChurn;
+
+    //Criando Constutor da classe
+    public ResultadoAnalise() {}
+
+    //Criando getters e setters
+    public int getPontuacao() {
+        return pontuacao;
+    }
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+    public String getClassificacao() {
+        return classificacao;
+    }
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+    private boolean isRiscoChurn() {
+        return riscoChurn;
+    }
+    private void setRiscoChurn(boolean riscoChurn) {
+        this.riscoChurn = riscoChurn;
+    }
+
+    //Criando metodos da classe
+    public String gerarRelatorio(){
+        return String.format("----Relatório da Reunião----\nPontuação: %d\nClassificação: %s\nRisco de Churn: %s", getPontuacao(), getClassificacao(), this.riscoChurn? "Sim" : "Não");
+    }
+
+    public void exibirResultado(){
+        gerarRelatorio();
+    }
 }
