da7f4fb1690dab923a358280dc810bcec9c35e50
João Vitor P.R. <jvprocha.05gmail.com>
2026-05-20 17:59:39 -0300

    Colocando variaveis na classe main

---
+++ src/br/com/fiap/main/Main.java
@@ -0,0 +1,11 @@
+package br.com.fiap.main;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome, email, cnpj, cpf, segmento, transcricao, classificacao;
+        int pontuacao;
+        LocalDate data;
+    }
+}
