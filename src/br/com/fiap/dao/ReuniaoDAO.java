c54864e873c4726b8352a95e0cf7012f9aa4e7ca
JVPR <jvprocha.05@gmail.com>
2026-09-09 14:14:29 -0300

    Updating ReuniaoDAO

--- src/br/com/fiap/dao/ReuniaoDAO.java
+++ src/br/com/fiap/dao/ReuniaoDAO.java
@@ -1,6 +1,10 @@
 package br.com.fiap.dao;
 
+import br.com.fiap.bean.Reuniao;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ReuniaoDAO {
 
@@ -11,4 +15,16 @@ public class ReuniaoDAO {
     public Connection getCon() {
         return con;
     }
+
+    public String inserir(Reuniao reuniao){
+        String sql = "insert into REUNIAO(DATA, TRANSCRICAO, ID_CLIENTE) values(?, ?, ?)";
+        try(PreparedStatement ps = getCon().prepareStatement(sql)) {
+
+        } catch (SQLException e) {
+            System.out.println();
+
+        }
+        return null;
+    }
+
 }
