9eacea6b90ece198d932594d292713deb7581c42
JVPR <jvprocha.05@gmail.com>
2026-09-08 22:20:58 -0300

    updating funcionario

--- src/br/com/fiap/bean/Funcionario.java
+++ src/br/com/fiap/bean/Funcionario.java
@@ -3,20 +3,37 @@ package br.com.fiap.bean;
 public class Funcionario extends Pessoa{
     //Criando atributo para classe Funcionario
     private String cpf;
+    private String senha;
+    private int idFuncionario;
 
     //Criando construtor da classe Funcionario
     public Funcionario() {}
-    public Funcionario(String nome, String email, String cpf){
+    public Funcionario(String cpf, String senha, int idFuncionario, String nome, String email) {
+        this.cpf = cpf;
+        this.senha = senha;
+        this.idFuncionario = idFuncionario;
         super.setNome(nome);
         super.setEmail(email);
-        setCpf(cpf);
     }
+
     //Criando getters e setters da classe Funcionario
     public String getCpf() {
         return cpf;
     }
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
     }
 
     //Puxando metodo da classe pai (Pessoa)
