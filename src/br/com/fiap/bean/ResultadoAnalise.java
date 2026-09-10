2928c60e41231fc0e9d411d54f85a66cef66f017
JVPR <jvprocha.05@gmail.com>
2026-09-10 18:31:57 -0300

    Updating ResultadoAnalise

--- src/br/com/fiap/bean/ResultadoAnalise.java
+++ src/br/com/fiap/bean/ResultadoAnalise.java
@@ -2,18 +2,39 @@ package br.com.fiap.bean;
 
 public class ResultadoAnalise {
     //Criando atributos da classe
+    private int idResultadoAnalise;
     private int pontuacao;
     private String classificacao;
+    private String sumario;
     private boolean riscoChurn;
+    private int idReuniao;
 
     //Criando Constutor da classe
     public ResultadoAnalise() {}
+
     public ResultadoAnalise(int pontuacao, String classificacao, boolean riscoChurn){
         setClassificacao(classificacao);
         setPontuacao(pontuacao);
         setRiscoChurn(riscoChurn);
     }
+
+    public ResultadoAnalise(int idResultadoAnalise, int pontuacao, String classificacao,
+                            String sumario, int idReuniao){
+        this.idResultadoAnalise = idResultadoAnalise;
+        setPontuacao(pontuacao);
+        setClassificacao(classificacao);
+        setSumario(sumario);
+        setIdReuniao(idReuniao);
+        setRiscoChurn("Risco de Churn".equals(classificacao));
+    }
+
     //Criando getters e setters
+    public int getIdResultadoAnalise() {
+        return idResultadoAnalise;
+    }
+    public void setIdResultadoAnalise(int idResultadoAnalise) {
+        this.idResultadoAnalise = idResultadoAnalise;
+    }
     public int getPontuacao() {
         return pontuacao;
     }
@@ -26,19 +47,52 @@ public class ResultadoAnalise {
     public void setClassificacao(String classificacao) {
         this.classificacao = classificacao;
     }
-    private boolean getRiscoChurn() {
+    public String getSumario() {
+        return sumario;
+    }
+    public void setSumario(String sumario) {
+        if (sumario != null && sumario.length() > 1500) {
+            sumario = sumario.substring(0, 1500);
+        }
+        this.sumario = sumario;
+    }
+    public boolean getRiscoChurn() {
         return riscoChurn;
     }
-    private void setRiscoChurn(boolean riscoChurn) {
+    public void setRiscoChurn(boolean riscoChurn) {
         this.riscoChurn = riscoChurn;
+    }
+    public int getIdReuniao() {
+        return idReuniao;
+    }
+    public void setIdReuniao(int idReuniao) {
+        this.idReuniao = idReuniao;
     }
 
     //Criando metodos da classe
     public String gerarRelatorio(){
-        return String.format("====Relatório da Reunião====\nPontuação: %d\nClassificação: %s\nRisco de Churn: %s", getPontuacao(), getClassificacao(), getRiscoChurn() ? "Sim" : "Não");
+        return String.format("""
+                ==== Relatório da Reunião ====
+                ID da análise: %d
+                ID da reunião: %d
+                Pontuação: %d
+                Classificação: %s
+                Risco de Churn: %s
+                Sumario: %s""",
+                getIdResultadoAnalise(),
+                getIdReuniao(),
+                getPontuacao(),
+                getClassificacao(),
+                getRiscoChurn() ? "Sim" : "Não",
+                getSumario() == null ? "(não informado)" : getSumario());
     }
 
     public void exibirResultado(){
         System.out.println(gerarRelatorio());
+    }
+
+    public String toString() {
+        return String.format("ResultadoAnalise [id=%d, pontuacao=%d, classificacao=%s, idReuniao=%d]",
+                idResultadoAnalise, pontuacao, classificacao, idReuniao);
     }
 }
