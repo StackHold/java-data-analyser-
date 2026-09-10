4fcdd9b0cafad6aa9d940db7487cbf48bf6a038a
JVPR <jvprocha.05@gmail.com>
2026-09-10 17:43:02 -0300

    Updating TesteMetodos

--- src/br/com/fiap/main/TesteMetodos.java
+++ src/br/com/fiap/main/TesteMetodos.java
@@ -1,12 +1,13 @@
 package br.com.fiap.main;
 
-import br.com.fiap.bean.Cliente;
-import br.com.fiap.bean.Funcionario;
-import br.com.fiap.bean.Pessoa;
+import br.com.fiap.bean.*;
+
+import java.time.LocalDate;
+import java.util.Map;
 
 public class TesteMetodos {
     public static void main(String[] args) {
-        System.out.println("============= 1°Teste: metodo validarCNPJ() =============\n");
+        System.out.println("\n============= 1°Teste: metodo validarCNPJ() =============\n");
         String cnpj = "12345678000195";
         String cnpj2 = "";
         String cnpj3 = "4556328792";
@@ -21,11 +22,12 @@ public class TesteMetodos {
         cliente3.setCnpj(cnpj3);
 
         System.out.println("Resultados:");
+        System.out.println("------------------------");
         System.out.println(cliente.validarCnpj(cnpj));
         System.out.println(cliente2.validarCnpj(cnpj2));
         System.out.println(cliente3.validarCnpj(cnpj3));
 
-        System.out.println("============= 2°Teste: metodo validarCPF() =============\n");
+        System.out.println("\n============= 2°Teste: metodo validarCPF() =============\n");
         String cpf  = "52998224725";
         String cpf2 = "529.982.247-25";
         String cpf3 = "390.533.447-05";
@@ -35,7 +37,7 @@ public class TesteMetodos {
         String cpf7 = "abcdefghijk";
 
         System.out.println("Resultados:");
-
+        System.out.println("------------------------");
         try {
             System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf));
         } catch (IllegalArgumentException e) {
@@ -79,7 +81,7 @@ public class TesteMetodos {
         }
 
 
-        System.out.println("============= 3°Teste: metodo validarEmail() =============\n");
+        System.out.println("\n============= 3°Teste: metodo validarEmail() =============\n");
         String email = "Astrogildo@gmail.com";
         String email2 = "@gmail.com";
         String email3 = "Astrogildo@";
@@ -98,12 +100,66 @@ public class TesteMetodos {
         pessoa4.setEmail(email4);
 
         System.out.println("Resultados:");
+        System.out.println("------------------------");
         System.out.println(pessoa.validarEmail(email));
         System.out.println(pessoa2.validarEmail(email2));
         System.out.println(pessoa3.validarEmail(email3));
         System.out.println(pessoa4.validarEmail(email4));
 
-        // ============= 4°Teste: metodo () ============= //
+        System.out.println("\n============= 4°Teste: metodo classificarReuniao() =============\n");
+        String transcricao  = "O cliente aprovou a proposta e quer fechar o contrato.";
+        String transcricao2 = "Apresentamos o Fluig da Totvs para a equipe.";
+        String transcricao3 = "Conversamos sobre o clima e remarcamos para a próxima semana.";
+        String transcricao4 = "O cliente comentou que está avaliando o SAP.";
+        String transcricao5 = "O cliente acha o valor caro.";
+        String transcricao6 = "Aprovaram a proposta, querem fechar a implementação do Fluig da Totvs, mas acharam caro e pensam em cancelar.";
 
+        Cliente clienteTeste = new Cliente("TOTVS SA", "contato@totvs.com", "12345678000195", "Tecnologia");
+
+        Reuniao reuniao  = new Reuniao(LocalDate.now(), transcricao,  clienteTeste);
+        Reuniao reuniao2 = new Reuniao(LocalDate.now(), transcricao2, clienteTeste);
+        Reuniao reuniao3 = new Reuniao(LocalDate.now(), transcricao3, clienteTeste);
+        Reuniao reuniao4 = new Reuniao(LocalDate.now(), transcricao4, clienteTeste);
+        Reuniao reuniao5 = new Reuniao(LocalDate.now(), transcricao5, clienteTeste);
+        Reuniao reuniao6 = new Reuniao(LocalDate.now(), transcricao6, clienteTeste);
+
+        Map<String, Categoria> palavras = Map.of(
+                "proposta",      Categoria.OPORTUNIDADE,
+                "fechar",        Categoria.OPORTUNIDADE,
+                "implementação", Categoria.OPORTUNIDADE,
+                "totvs",         Categoria.PRODUTO_TOTVS,
+                "fluig",         Categoria.PRODUTO_TOTVS,
+                "sap",           Categoria.CONCORRENTE,
+                "senior",        Categoria.CONCORRENTE,
+                "caro",          Categoria.RISCO_CHURN,
+                "cancelar",      Categoria.RISCO_CHURN,
+                "insatisfeito",  Categoria.RISCO_CHURN
+        );
+
+        AnalisadorTranscricao analisador = new AnalisadorTranscricao();
+        analisador.setPalavraChave(palavras);
+
+        System.out.println("Resultados:");
+        System.out.println("------------------------");
+        ResultadoAnalise resultado  = analisador.processar(reuniao);
+        System.out.println(resultado.getPontuacao() + " -> " + resultado.getClassificacao());
+
+        ResultadoAnalise resultado2 = analisador.processar(reuniao2);
+        System.out.println(resultado2.getPontuacao() + " -> " + resultado2.getClassificacao());
+
+        ResultadoAnalise resultado3 = analisador.processar(reuniao3);
+        System.out.println(resultado3.getPontuacao() + " -> " + resultado3.getClassificacao());
+
+        ResultadoAnalise resultado4 = analisador.processar(reuniao4);
+        System.out.println(resultado4.getPontuacao() + " -> " + resultado4.getClassificacao());
+
+        ResultadoAnalise resultado5 = analisador.processar(reuniao5);
+        System.out.println(resultado5.getPontuacao() + " -> " + resultado5.getClassificacao());
+
+        ResultadoAnalise resultado6 = analisador.processar(reuniao6);
+        System.out.println(resultado6.getPontuacao() + " -> " + resultado6.getClassificacao());
+
+        ResultadoAnalise resultado7 = analisador.processar(reuniao);
+        System.out.println(resultado7.getPontuacao() + " -> " + resultado7.getClassificacao());
     }
 }
