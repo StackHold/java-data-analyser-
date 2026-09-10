1679577db65e12933efab47f2a678a93a5f8967d
JVPR <jvprocha.05@gmail.com>
2026-09-10 17:25:32 -0300

    Updating TesteMetodos

--- src/br/com/fiap/main/TesteMetodos.java
+++ src/br/com/fiap/main/TesteMetodos.java
@@ -1,6 +1,7 @@
 package br.com.fiap.main;
 
 import br.com.fiap.bean.Cliente;
+import br.com.fiap.bean.Funcionario;
 import br.com.fiap.bean.Pessoa;
 
 public class TesteMetodos {
@@ -19,12 +20,63 @@ public class TesteMetodos {
         Cliente cliente3 = new Cliente();
         cliente3.setCnpj(cnpj3);
 
-        System.out.println("======== Resultados ========");
+        System.out.println("Resultados:");
         System.out.println(cliente.validarCnpj(cnpj));
         System.out.println(cliente2.validarCnpj(cnpj2));
         System.out.println(cliente3.validarCnpj(cnpj3));
 
-        // ============= 2°Teste: metodo validarCPF() ============= //
+        System.out.println("============= 2°Teste: metodo validarCPF() =============\n");
+        String cpf  = "52998224725";
+        String cpf2 = "529.982.247-25";
+        String cpf3 = "390.533.447-05";
+        String cpf4 = "529.982.247-26";
+        String cpf5 = "111.111.111-11";
+        String cpf6 = "123456789";
+        String cpf7 = "abcdefghijk";
+
+        System.out.println("Resultados:");
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf2));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf3));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf4));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf5));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf6));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("VÁLIDO: " + Funcionario.validaCpf(cpf7));
+        } catch (IllegalArgumentException e) {
+            System.out.println("REJEITADO: " + e.getMessage());
+        }
 
 
         System.out.println("============= 3°Teste: metodo validarEmail() =============\n");
@@ -45,7 +97,7 @@ public class TesteMetodos {
         Pessoa pessoa4 = new Pessoa();
         pessoa4.setEmail(email4);
 
-        System.out.println("======== Resultados ========");
+        System.out.println("Resultados:");
         System.out.println(pessoa.validarEmail(email));
         System.out.println(pessoa2.validarEmail(email2));
         System.out.println(pessoa3.validarEmail(email3));
