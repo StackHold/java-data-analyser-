f2c58fb146da4b8e9753fd6f212183ba6776ab39
JVPR <jvprocha.05@gmail.com>
2026-09-10 09:05:38 -0300

    Inserindo primeiro método de relevancia

--- src/br/com/fiap/main/TesteMetodos.java
+++ src/br/com/fiap/main/TesteMetodos.java
@@ -4,9 +4,24 @@ import br.com.fiap.bean.Cliente;
 
 public class TesteMetodos {
     public static void main(String[] args) {
+        // ============= Teste do metodo validarCNPJ() =============
         String cnpj = "12345678000195";
+        String cnpj2 = "";
+        String cnpj3 = "4556328792";
+
         Cliente cliente = new Cliente();
         cliente.setCnpj(cnpj);
+
+        Cliente cliente1 = new Cliente();
+        cliente1.setCnpj(cnpj2);
+
+        Cliente cliente2 = new Cliente();
+        cliente2.setCnpj(cnpj3);
+
+        // ======== Resultados ========
         System.out.println(cliente.validarCnpj(cnpj));
+        System.out.println(cliente1.validarCnpj(cnpj2));
+        System.out.println(cliente2.validarCnpj(cnpj3));
+
     }
 }
