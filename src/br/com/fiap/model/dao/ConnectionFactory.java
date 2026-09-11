ac38415fc051cf3df8e5352105e067b1b95c32e3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-11 10:41:43 -0300

    Update final

--- src/br/com/fiap/model/dao/ConnectionFactory.java
+++ src/br/com/fiap/model/dao/ConnectionFactory.java
@@ -10,8 +10,8 @@ public class ConnectionFactory {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-            final String USER = "rm562719";
-            final String PASS = "110905";
+            final String USER = "########";
+            final String PASS = "######";
             con = DriverManager.getConnection(url, USER, PASS);
         } catch (ClassNotFoundException e) {
             System.out.println("ERRO: Não foi possivel encontrar a classe de conexão!" + e.getMessage());
