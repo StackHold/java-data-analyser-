785969ef78a5c1b42cae4b8bbb96355a0b7d25e6
JVPR <jvprocha.05@gmail.com>
2026-09-17 20:14:44 -0300

    Updating sprint 3

--- src/br/com/fiap/model/dao/ConnectionFactory.java
+++ src/br/com/fiap/model/dao/ConnectionFactory.java
@@ -10,8 +10,8 @@ public class ConnectionFactory {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-            final String USER = "########";
-            final String PASS = "######";
+            final String USER = "rm562719"; //USER: rm562719
+            final String PASS = "110905"; //PASS: 110905
             con = DriverManager.getConnection(url, USER, PASS);
         } catch (ClassNotFoundException e) {
             System.out.println("ERRO: Não foi possivel encontrar a classe de conexão!" + e.getMessage());
